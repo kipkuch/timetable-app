860ae688311bd83b958fef39ee7b764861b61168
kipkuch <robert.ngetich@gmail.com>
2024-11-20 15:14:23 +0100

    Added days to the grid

--- src/app/timetable/timetable.component.ts
+++ src/app/timetable/timetable.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-timetable',
   standalone: true,
-  imports: [ MatSlideToggleModule ],
+  imports: [ MatGridListModule ],
   templateUrl: './timetable.component.html',
   styleUrl: './timetable.component.scss'
 })
